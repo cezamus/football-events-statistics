6cf7424f48eade1835a98976a4f60dce7dfa32a4
cezamus <cezary.m.muszynski@gmail.com>
2024-07-10 20:47:16 +0200

    - remove unused method

--- src/main/java/dev/cm/football_events_statistics/repository/TeamRepository.java
+++ src/main/java/dev/cm/football_events_statistics/repository/TeamRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 @Repository
 public interface TeamRepository extends JpaRepository<TeamStatistics, Long> {
 
-    boolean existsByName(String name);
-
     Optional<TeamStatistics> getTeamStatisticsByName(String name);
 }
